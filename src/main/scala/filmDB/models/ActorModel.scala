3360b079e96c851de2ec6fe57bcc5edb95e4cadd
Leonardo Evic <evicleonardo@gmail.com>
2025-09-23 11:42:15 +0200

    actors model (not finished yet)

--- src/main/scala/filmDB/models/ActorModel.scala
+++ src/main/scala/filmDB/models/ActorModel.scala
@@ -1,5 +1,32 @@
 package filmDB.models
 
-object ActorModel {
+import filmDB.datatypes.Actor
 
+object ActorModel {
+    private val actors: ListBuffer[Actor] = ListBuffer()
+
+
+  /**
+   * Add an actor to the model
+   * @param id
+   * @param name
+   * @param bornYear
+   * @param diedYear
+   * @return Int
+   */
+  def addActor(id: Int, name: String, bornYear: Int, diedYear: Option[Int]): Int =
+    this.actors.addOne(Actor(id, name, bornYear, diedYear))
+    id
+
+  
+
+  /**
+   * Get actors by a filter or without filter
+   * @param filter
+   * @return
+   */
+  def getActors(filter: Option[(Actor => Boolean)]): List[Actor] =
+    filter match
+      case None => this.actors.toList
+      case Some(f) => this.actors.filter(f).toList
 }
